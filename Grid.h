22757409a196982461cacd8e67204c42e1961eb8
Norman Bemelmans <nlbemelmans@gmail.com>
2023-04-13 21:25:03 -0400

    added some new declarations and methods.

--- Grid.h
+++ Grid.h
@@ -1,19 +1,23 @@
 //Grid class header file
-//10 April 2023
 //Grid holds a cellmap for use in Conway's game of life.
+//13 April 2023
+//Summary of changes: updated declaration of static const int data members.
+
 
 #ifndef GAME_OF_LIFE_H
 #define GAME_OF_LIFE_H
 
 class Grid
 {
-	static constexpr int GRID_HEIGHT;
-	static constexpr int GRID_WIDTH;
-	static constexpr char LIVE_SYMBOL;
-	static constexpr char DEAD_SYMBOL;
+	static const int GRID_HEIGHT = 10;
+	static const int GRID_WIDTH = 10;
+	static const char LIVE_SYMBOL;
+	static const char DEAD_SYMBOL;
 	constexpr int LENGTH;
+	void read_grid(g);
 
 	public:
+	Grid();
 	char g[LENGTH];
 	void printGrid();
 	int neighborCount();
